71012d00b8af5fe1eb1cb6835bb478bec2d9f192
chidokato <tuan.pn92@gmail.com>
2025-05-05 14:31:17 +0700

    sdf

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -136,7 +136,107 @@
 <!-- @include('pages.section.overview') -->
 @include('pages.section.outstanding')
 @include('pages.section.location')
-@include('pages.section.utilities')
+
+<section class="potential">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 ">
+                <div class="title gradient-text mb-4">
+                    Tiềm năng gia tăng giá trị vượt trội <br> đón đầu tương lai
+                </div>
+                <div class="desc">
+                    <div class="img">
+                        <img  src="assets/images/58.png">
+                    </div>
+                    <div class="content">
+                        <p>Sở hữu vị trí tâm điểm đón đầu chu kỳ phát triển hạ tầng & công nghiệp của Phổ Yên, Yên Bình Complex nắm chắc tiềm năng tăng giá "phi mã". Lợi thế nhân đôi khi là căn hộ thương mại tiên phong tại "thủ phủ công nghệ", đón đầu nhu cầu ở khổng lồ của hàng trăm nghìn chuyên gia Samsung & KCN, mở ra cơ hội đầu tư kép vượt trội: vừa gia tăng giá trị tài sản, vừa khai thác cho thuê với lợi nhuận bền vững.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="utilities" id="tien-ich">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="img mb-4">
+                    <img class="width80" src="assets/images/16.png">
+                    <div class="title">TIỆN ÍCH ĐẶC QUYỀN</div>
+                </div>
+                <div class="desc">
+                    <p>Khám phá đặc quyền tận hưởng thế giới tiện ích hoàn mỹ ngay ngưỡng cửa, nơi nuông chiều mọi giác quan, mang đến cuộc sống tiện nghi viên mãn và nâng tầm giá trị mỗi ngày</p>
+                    <img src="assets/images/17.png">
+                </div>
+            </div>
+            <div class="col-md-4 pt-5">
+                <div class="img mt-4">
+                    <img class="w-100" src="assets/images/15.png">
+                </div>
+                <div class="content">
+                    <ol class="fancy-counter">
+                        <li>Quảng Trường Bậc Cấp</li>
+                        <li>Khu BBQ ngoài trời</li>
+                        <li>Đường dạo chạy bộ ven hồ</li>
+                        <li>Màng xanh cảnh quan</li>
+                        <li>Khu thư giãn, vọng cảnh</li>
+                        <li>Khu sân chơi đa năng</li>
+                        <li>Đường dạo bậc bước BTXM</li>
+                        <li>Quảng trường trung tâm</li>
+                        <li>Bãi cỏ đa năng</li>
+                        <li>Khu vui chơi trẻ em</li>
+                        <li>Tổ hợp sân thể thao</li>
+                        <li>Cung đường thể thao</li>
+                        <li>Vườn cờ</li>
+                    </ol>
+
+                    <ol class="fancy-counter fancy-counter-14">
+                        <li>Sân thiền, Yoga ngoài trời</li>
+                        <li>Đường dạo kết nối</li>
+                        <li>Quảng trường clubhouse</li>
+                        <li>Trục quảng trường ánh sáng</li>
+                        <li>Sân vui chơi trẻ em</li>
+                        <li>Sân tập Yoga</li>
+                        <li>Khu nghỉ tĩnh</li>
+                        <li>Sân thể thao</li>
+                        <li>Bãi cỏ đa năng</li>
+                        <li>Sân chơi âm nhạc</li>
+                        <li>Giàn hoa cảnh quan</li>
+                        <li>Bãi đỗ xe</li>
+                        <li>Trường Học</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+
+        
+    </div>
+</section>
+
+<section class="utilities-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="title mb-2">HỆ THỐNG GIÁO DỤC TOÀN DIỆN</div>
+                <p>Trường mầm non, tiểu học, THCS... hiện đại với đội ngũ giáo viên chuyên nghiệp.</p>
+                <div class="img"><img src="assets/images/46.png"></div>
+            </div>
+            <div class="col-md-6">
+                <div class="img"><img src="assets/images/45.png"></div>
+                <div class="title mt-4">HỆ THỐNG TRUNG TÂM THƯƠNG MẠI VÀ GIẢI TRÍ</div>
+                <p>Nhà hàng Á, Âu, Việt</p>
+                <p>Trung tâm thương mại mua sắm</p>
+                <p>Hệ thống chuỗi Shophouse kinh doanh, khu BBQ, Gym, Spa, Sân Tennis, Karaoke ….</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<hr class="line">
+
+<!-- @include('pages.section.utilities') -->
 @include('pages.section.premises')
 @include('pages.section.apartment')
 @include('pages.section.policy')
