9ed8f7f37392c1fe10136c595fee59b25d1db24f
chidokato <tuan.pn92@gmail.com>
2025-05-05 16:16:42 +0700

    sdf

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -97,7 +97,7 @@
                     <img src="assets/images/6.png">
                     <div class="iteam">
                         <p>MẬT ĐỘ XÂY DỰNG:</p>
-                        <p>~46.4%</p>
+                        <p>46.4%</p>
                     </div>
                 </div>
             </div>
